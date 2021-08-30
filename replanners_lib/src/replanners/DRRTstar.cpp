ec0c2b10bc0de41b35d88b1bfb1f70ad6d343ed7
Cesare Tonola <c.tonola001@unibs.it>
2021-08-30 18:45:24 +0200

    no discardParents

--- replanners_lib/src/replanners/DRRTstar.cpp
+++ replanners_lib/src/replanners/DRRTstar.cpp
@@ -56,7 +56,9 @@ bool DynamicRRTstar::connectBehindObs(NodePtr& node)
   NodePtr replan_goal;
   if(!nodeBehindObs(replan_goal)) return false;
 
-  replan_goal->disconnectParentConnections();
+  ROS_INFO_STREAM(*replan_goal);
+
+  //  replan_goal->disconnectParentConnections();
 
   double radius = 1.5*((replan_goal->getConfiguration()-node->getConfiguration()).norm());
   InformedSampler sampler (node->getConfiguration(),node->getConfiguration(),lb_,ub_,radius);
@@ -67,39 +69,53 @@ bool DynamicRRTstar::connectBehindObs(NodePtr& node)
   tree->changeRoot(node);
   tree->rewireOnlyWithPathCheck(node,checked_connections,radius,2);
 
-  ROS_WARN("QUAAAAAAAA");
   //*  STEP 2: ADDING NEW NODES AND SEARCHING WITH RRT*  *//
   ros::WallTime toc = ros::WallTime::now();
-  while(((toc-tic).toSec()-max_time_)>0.0)
+  while(max_time_-((toc-tic).toSec())>0.0)
   {
     NodePtr new_node;
     Eigen::VectorXd q=sampler.sample();
 
     if (tree->rewireWithPathCheck(q,checked_connections,radius,new_node))
     {
-      if ((new_node->getConfiguration()-replan_goal->getConfiguration()).norm()<1e-03)
-      {
-        if(checker_->checkPath(new_node->getConfiguration(),replan_goal->getConfiguration()))
-        {
-          double cost = metrics_->cost(new_node->getConfiguration(),replan_goal->getConfiguration());
-          Connection conn = Connection(new_node,replan_goal);
-          conn.setCost(cost);
-          conn.add();
+      ROS_INFO("rewire ok");
 
-          break;
+      if(!success_)  //if success, i should not try to connect to goal but only rewire to improve the path
+      {
+        if ((new_node->getConfiguration()-replan_goal->getConfiguration()).norm()<1e-04)
+        {
+
+          ROS_INFO("DISTANZA RIDOTTA");
+          if(checker_->checkPath(new_node->getConfiguration(),replan_goal->getConfiguration()))
+          {
+            double cost = metrics_->cost(new_node->getConfiguration(),replan_goal->getConfiguration());
+            Connection conn = Connection(new_node,replan_goal);
+            conn.setCost(cost);
+            conn.add();
+
+            success_ = true;
+
+            break;
+          }
         }
       }
     }
     toc = ros::WallTime::now();
   }
 
-  PathPtr connecting_path = std::make_shared<Path>(tree->getConnectionToNode(replan_goal),metrics_,checker_);
+  if(success_)
+  {
+    PathPtr connecting_path = std::make_shared<Path>(tree->getConnectionToNode(replan_goal),metrics_,checker_);
+    std::vector<ConnectionPtr> new_connections = connecting_path->getConnections();
 
-  std::vector<ConnectionPtr> new_connections = connecting_path->getConnections();
-  std::vector<ConnectionPtr> subpath_connections = current_path_->getSubpathFromNode(replan_goal)->getConnections();
-  new_connections.insert(new_connections.end(),subpath_connections.begin(),subpath_connections.end());
+    if(replan_goal->getConfiguration() != current_path_->getWaypoints().back())
+    {
+      std::vector<ConnectionPtr> subpath_connections = current_path_->getSubpathFromNode(replan_goal)->getConnections();
+      new_connections.insert(new_connections.end(),subpath_connections.begin(),subpath_connections.end());
+    }
 
-  replanned_path_ = std::make_shared<Path>(new_connections,metrics_,checker_);
+    replanned_path_ = std::make_shared<Path>(new_connections,metrics_,checker_);
+  }
 
   return success_;
 }
@@ -111,7 +127,7 @@ bool DynamicRRTstar::replan()
     ConnectionPtr conn = current_path_->findConnection(current_configuration_);
     NodePtr node_replan = current_path_->addNodeAtCurrentConfig(current_configuration_,conn,true);
 
-    ROS_INFO_STREAM("N PARENTS: "<<node_replan->getParents().size()<<"N CHILDREN: "<<node_replan->getChildren().size());
+    ROS_INFO_STREAM("Conf: "<<node_replan->getConfiguration()<<" N PARENTS: "<<node_replan->getParents().size()<<" N CHILDREN: "<<node_replan->getChildren().size());
 
     connectBehindObs(node_replan);
   }
