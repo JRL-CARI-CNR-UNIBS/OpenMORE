8e1236282efad7ac80a8442575926646849e1e62
Cesare Tonola <c.tonola001@unibs.it>
2021-09-01 20:10:32 +0200

    try fixing the parent conn of replan_goal

--- replanners_lib/src/replanners/DRRTstar.cpp
+++ replanners_lib/src/replanners/DRRTstar.cpp
@@ -123,7 +123,7 @@ bool DynamicRRTstar::connectBehindObs(NodePtr& node)
 
         ROS_INFO("DOPO REWIRE");
 
-//        if(disp_ != NULL) disp_->displayNode(new_node);
+        //        if(disp_ != NULL) disp_->displayNode(new_node);
 
         if(replan_goal->getParents().at(0) == new_node)
         {
@@ -144,14 +144,22 @@ bool DynamicRRTstar::connectBehindObs(NodePtr& node)
               }
               if(cont>0)ROS_INFO_STREAM("IN1 WHILE NODES DISCONNECTED: "<<cont);
 
-              ROS_INFO_STREAM("replan_goal: "<<*replan_goal);
-              ROS_INFO_STREAM("parent connections:"<<replan_goal->getParents().size());
+
+              std::vector<ConnectionPtr> conn2new_node;
               if(replan_goal->getParents().size()>0)
               {
-                //SISTEMA
-                replan_goal->parent_connections_.clear();
-                std::vector<ConnectionPtr> conn2new_node = tree->getConnectionToNode(new_node);
-                std::reverse(conn2new_node.begin(), conn2new_node.end());
+                replan_goal->parent_connections_.clear();  //remove the old parent connections because now the parents of replan_goal come frome new_node
+                conn2new_node = new_node->parent_connections_;
+              }
+
+              double cost = metrics_->cost(new_node->getConfiguration(),replan_goal->getConfiguration());
+              ConnectionPtr conn = std::make_shared<Connection>(new_node,replan_goal);
+              conn->setCost(cost);
+
+              conn->add(); //add connection between new_node and replan_goal as first connection in parent_connections of replan_goal
+
+              if(!conn2new_node.empty())  //and THEN add all the connections from start to new_node in parent_connections of replan_goal
+              {
                 replan_goal->parent_connections_.insert(replan_goal->parent_connections_.end(),conn2new_node.begin(),conn2new_node.end());
               }
 
@@ -161,22 +169,7 @@ bool DynamicRRTstar::connectBehindObs(NodePtr& node)
                 if(tree_node->getParents().size() == 0 && tree_node->getChildren().size() == 0)
                   cont +=1;
               }
-              if(cont>0)
-              {
-                ROS_INFO_STREAM("IN2 WHILE NODES DISCONNECTED: "<<cont);
-                disp_->displayTree(tree);
-                int t=0;
-                while(t<100)
-                {
-                  t+=1;
-                  ros::Duration(1).sleep();
-                }
-              }
-
-              double cost = metrics_->cost(new_node->getConfiguration(),replan_goal->getConfiguration());
-              ConnectionPtr conn = std::make_shared<Connection>(new_node,replan_goal);
-              conn->setCost(cost);
-              conn->add();
+              if(cont>0) ROS_INFO_STREAM("IN2 WHILE NODES DISCONNECTED: "<<cont);
 
               success_ = true;
             }
